74a3caa1338675c391352916219ab59cd8e4241d
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2024-06-20 18:54:12 +0500

    design and responsive form

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -6,6 +6,6 @@ class ContactFormsController < ApplicationController
   def create
     @contact_form = params.require(:contact_form).permit(:name, :pesel, :email, :phone, :message, :district, :referrer, :forum_nickname)
     ContactFormMailer.with(contact_form: @contact_form).send_form.deliver_now
-    redirect_to root_path, notice: 'Formularz został wysłany.'
+    redirect_to root_path, notice: 'Twoja wiadomość została wysłana.'
   end
 end

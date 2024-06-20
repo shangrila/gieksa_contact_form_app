02003e8a26ec22f4043f4a3ac62c7c1aef232ee5
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2024-06-20 19:14:25 +0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
+  domain:               'gmail.com',
   user_name:            '<your_email>',
   password:             '<your_password>',
   authentication:       'plain',
